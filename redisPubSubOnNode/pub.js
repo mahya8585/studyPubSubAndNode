6e0edc7bd1c9c020792ff4e7d75cd822a5c95964
ishida.m <ishida.m@opst.co.jp>
2016-08-15 12:05:38 +0900

    ディレクトリ構成の変更

--- redisPubSubOnNode/pub.js
+++
@@ -1,7 +0,0 @@
-var publisher = require('redis').createClient(【PORT NUMBER】,【HOSTNAME】, {auth_pass: 【AUTH】, tls: {servername: 【HOSTNAME】】}});
-
-//publishするチャネル名＋送信する文字列
-publisher.publish('test', 'FROM pub.js!!!!');
-
-
-//CLIか何かでsubしてね
--- redisPubSubOnNode/sub.js
+++
@@ -1,9 +0,0 @@
-var subscriber = require('redis').createClient(【PORT NUMBER】,【HOSTNAME】, {auth_pass: 【AUTH】, tls: {servername: 【HOSTNAME】】}});
-
-//subscribeするチャネルの設定
-subscriber.subscribe('test');
-subscriber.on('message', function(channel, message) {
-	console.log('channel: ' + channel + ', message: ' + message);
-});
-
-//CLIか何かでpubしてね
